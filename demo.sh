7bb69f93ab9cd533049df8d9518396fe8bb73f0d
gaoxuewei <834990270@qq.com>
2018-11-28 22:40:48 +0800

    自动制作网页脚本

--- demo.sh
+++ demo.sh
@@ -1,23 +1,19 @@
-﻿if [ -d $1 ]; then
-  echo '$1 已经存在'
-  exit 1
+if [ -d $1 ]; then
+  echo 'error: dir exists'
+  exit
 else
+
 mkdir $1
 cd $1
 mkdir css js
 touch index.html css/style.css js/main.js
-echo "<!DOCTYPE html>
-<html lang="en">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <script type="text/javascript" src="js/main.js"></script>
-    <title>hello</title>
-    <h1>Hi</h1>
-</html>" >> index.html
+echo "<!DOCTYPE><title>Hello</title><link rel="stylesheet" href="css/style.css"><script type="text/javascript" src="js/main.js"></script><h1>Hi</h1></html>" >> index.html
   echo "h1{color: red;}" >> css/style.css
   echo "var string = 'Hello World'; alert(string);" >> js/main.js
+
   echo 'success'
-exit 0
+
+  exit 
+
 fi
+
