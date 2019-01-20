0c01fdf4df6b7956460b7daebec6dea436fbaafb
gaoxuewei <834990270@qq.com>
2019-01-20 12:30:59 +0800

    promise优化

--- nodejs-test/main.js
+++ nodejs-test/main.js
@@ -1,21 +1,44 @@
+window.$ = window.jQuery
 
-myButton.addEventListener('click', (e)=>{
-    window.jQuery.ajax(
-        {
-            url:'',
-            method:'get',
+window.Promise =function(fn){
+    //...
+    return {then:function(){}}
+}
+window.jQuery.ajax = function({url,method,body,,headers}){
+
+    return new promise(function(resolve,reject){
+        let request = new XMLHttpRequest()
+        request.open(method,url)//配置request
+        for(let key in headers){
+            let value =headers[key]
+            request.setRequestHeader(key,value)
         }
-    )
-}).then(//then的中文含义：然后
-    (responseText)=>{console.log(responseText);
-        return responseText
-    },
-    (request)=>{console.log('error1');
-        return '已处理'
-    }
-).then(
-    (上一次的处理结果)=>{上一次的处理结果},
-    (request)=>{console.log('error2')}
-)
+        request.onreadystatechange = ()=>{
+            if(request.readyState === 4){
+                if(request.status >= 200 && request.status<300){
+                    resolve.call(undefined,request.responseText)
+                }else if(request.status >= 400){
+                    reject.call(undefined,request)
+                }
+            }
+        }
+        request.send(body)
+    })
+}
 
-
+function f1(responseText){}
+function f2(responseText){}
+window.$ =window.jQuery
+myButton.addEventListener('click', (e)=>{
+    let promise=window.jQuery.ajax(
+        {
+            url:'/xxx',
+            method:'get',
+            headers:{'Content-Type':'application/x-www-form-urlencoded',
+                    'frank':'18'
+            },
+        })
+        promise.then((text)=>{console.log(text)},
+        (request)=>{console.log(request)}
+        )
+})
